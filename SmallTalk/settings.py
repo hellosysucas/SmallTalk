229f1321acf3a8d3bce0171215be41af7888d6f7
lc <631979719@qq.com>
2013-07-15 09:52:12 +0800

    database part of login and  manage friends finished

--- SmallTalk/settings.py
+++ SmallTalk/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': './sqlite3.db',                      # Or path to database file if using sqlite3.
+        'NAME': 'E:\\projects\\SmallTalk\\sqlite3.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -111,7 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	'G:\\大三下学期\\软件综合实训\\test1\\SmallTalk\\templates',
+	'E:\\projects\\SmallTalk\\templates',
 )
 
 INSTALLED_APPS = (
@@ -156,5 +156,5 @@ LOGGING = {
         },
     }
 }
-STATIC_PATH='./static'
+STATIC_PATH='E:\\projects\\SmallTalk\\static'
 
--- mytalk/views.py
+++ mytalk/views.py
@@ -5,48 +5,114 @@ from urllib import unquote
 from django.template.loader import get_template
 from django.shortcuts import render_to_response
 from django.core.context_processors import csrf
-import datetime
+import datetime, re
 from django import forms
 from django.http import HttpResponseRedirect
 from mytalk.models import User,Store,Label,Comment,Reply,LabelInline,StoreAdmin
 
 #判断用户名是否格式合法
 def is_uid_name_valid(uid):
-    return True
+    pattern = re.compile(r'[A-Za-z](\w|_| )*')
+    
+    if pattern.match(uid) and len(uid) <= 20:
+        return True
+    return False
     
 #判断邮箱格式是否合法
 def is_email_name_valid(email):
-    return True
+    pattern = re.compile(r'\w*@(\w)+.(com|cn|org)')
+    
+    if pattern.match(email) and len(email) <= 20:
+        return True
+    return False
 
-#更新用户id为新的newid,true代表成功,需要验证用户名id的格式是否合法
-def update_user_id(uid,newUid):
-    return True
-
-#更新用户uid的邮箱为email,需要验证email的格式是否合法
-def update_user_email(uid,email):
-    return True
-
-#更新用户uid的密码为新的密码password
-def update_user_password(uid,password):
-    return True
+#判断密码是否合法
+def is_password_valid(password):
+    pattern = re.compile(r'\w+')
+    
+    if pattern.match(password) and len(password) <= 20:
+        return True
+    return False
 
 #判断某个用户名是否已经存在
 def is_uid_exist(uid):
+    user = User.objects.filter(id = uid)
+    if len(user) == 1:
+        return True  
     return False
 
 #判断用户是否合法,需要先验证uid和upassword是否为空等格式是否正确
 def is_user_valid(uid,upassword):
-    return True
+    if is_uid_name_valid(uid) and is_password_valid(upassword) and is_uid_exist(uid):
+        user = User.objects.get(id = uid)
+        if user.password == upassword:
+            return True
+    return False
+
+#更新用户id为新的newid,true代表成功,需要验证用户名id的格式是否合法
+def update_user_id(uid,newUid):
+    try:
+        user = User.objects.get(id = uid)
+        
+        if is_uid_exist(newUid) == False and is_uid_name_valid(newUid):
+            user.id = newUid
+            user.save()
+            print "save id"
+            User.objects.filter(id = uid).delete()
+            return True
+    except:
+        print "more than one or does not exit"    
+        
+    return False
+
+#更新用户uid的邮箱为email,需要验证email的格式是否合法
+def update_user_email(uid,email):
+    try:
+        user = User.objects.get(id = uid)
+        if is_email_name_valid(email):
+            user.email = email
+            user.save()
+            print "save email"
+            return True               
+    except:
+        print uid + "more than one or does not exit"
+    
+    return False
+
+#更新用户uid的密码为新的密码password
+def update_user_password(uid, password):
+    try:
+        user = User.objects.get(id = uid)
+        if is_password_valid(password):
+            user.password = password
+            user.save()
+            print "save password"
+            return True
+    except:
+        print uid + "more than one or does not exit"
+    return False
 
 #获得用户信息,包括用户邮箱Email
 def getUserMessage(uid):
-    message = {'uid':uid,'email':'123@qq.com'}
-    return message
+    try:
+        user = User.objects.get(id = uid)
+        return {'uid':user.id, 'email':user.email}
+    except:
+        return {'uid':uid, 'email':''}
 
 #获得用户uid的好友列表
 def getFriendsList(uid):
-    myFriendsObj= ['aa','bb','cc','dd']
-    return myFriendsObj
+    friends = []
+    try:
+        user = User.objects.get(id = uid)
+        friendsList = user.friends.all()
+        
+        print friendsList
+        for i in friendsList:
+            friends.append(i.id)
+    except:
+        print uid + "more than one or does not exit"
+    return friends
 
 '''主页，登录时设置session为用户 uid,对应于数据库中的id'''
 def index(request):
@@ -74,7 +140,7 @@ def friends(request):
     myFriendsObj = getFriendsList(uid)
     
     return render_to_response('friends.html',{'uid':uid,'friendsList':myFriendsObj})
-    
+        
 '''更改用户信息'''
 def exchangeUserMessage(request):
     uid = ''
@@ -83,27 +149,29 @@ def exchangeUserMessage(request):
     
     uid = request.session.get('uid')
     userMessage = getUserMessage(uid)
-        
+    is_update = True
+    
     if request.POST.get('username')!=None and request.POST.get('email')!=None:
         #return HttpResponse(request.POST.get('updatePassword') != request.POST.get('password'))
         message = { 'uid':request.POST.get('username'),'email':request.POST.get('email'),'isEmail':' ','isPassword':' ','isUsername':' '}
-        if userMessage['email']!= request.POST.get('email'):
-            if is_email_name_valid(request.POST.get('email')) == False:
-                message['isEmail'] = '邮箱不合法'
-            elif update_user_email(uid,request.POST.get('email')) == False:
-                message['isEmail'] = '邮箱不合法'
-  
-        if userMessage['uid']!= request.POST.get('username'):
-            if is_uid_name_valid(request.POST.get('username')) == False:
-                message['isUsername'] = '用户名不合法'
-            elif update_user_id(uid,request.POST.get('username')) == False:
-                message['isUsername'] = '用户名不合法'
-            else:
-                request.session['uid'] = request.POST.get('username')
-                uid = request.POST.get('username')
-                
-        if request.POST.get('password')!=request.POST.get('updatePassword'):
+        if userMessage['email']!= request.POST.get('email') and is_email_name_valid(request.POST.get('email')) == False:
+            message['isEmail'] = '邮箱不合法'
+            is_update = False
+        
+        if userMessage['uid']!= request.POST.get('username') and (is_uid_name_valid(request.POST.get('username')) == False or is_uid_exist(request.POST.get('username'))):
+            message['isUsername'] = '用户名不合法或用户名已经存在'
+            is_update = False
+            
+        if request.POST.get('password')!=request.POST.get('updatePassword') or is_password_valid(request.POST.get('password')) == False:
             message['isPassword'] = '密码不合法,或前后密码不一致'
+            is_update = False
+            
+        if is_update:
+            update_user_email(uid,request.POST.get('email')) 
+            update_user_password(uid,  request.POST.get('password'))
+            if update_user_id(uid, request.POST.get('username')):
+                uid =  request.session['uid'] = request.POST.get('username')
+        
         return render_to_response('exchangeUserMessage.html',{'uid':uid,'message':message})
         
     userMessage = getUserMessage(uid)    
