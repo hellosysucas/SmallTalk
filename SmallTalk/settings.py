6db6eec2f7feb7eedb7f0ef349304f7e8dd21d51
haoaina520 <haoaina520@gmail.com>
2013-07-16 21:53:30 +0800

    调整目录结构

--- SmallTalk/settings.py
+++ SmallTalk/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'E:\\projects\\SmallTalk\\sqlite3.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/haoaina521/Workplace/workplace_aptana/SmallTalk/sqlite3.db',# Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -156,5 +156,5 @@ LOGGING = {
         },
     }
 }
-STATIC_PATH='E:\\projects\\SmallTalk\\static'
+
 
